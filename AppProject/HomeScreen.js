bc30eabb4cdda94b52d15125b36a8472734f839a
evanja57 <evanja57@gmail.com>
2023-05-14 11:18:39 -0700

    spelling

--- AppProject/HomeScreen.js
+++ AppProject/HomeScreen.js
@@ -72,7 +72,7 @@ export default function HomeScreen({navigation}){
             </View>
 
             <AppButton
-            title='Search Recipies'
+            title='Search Recipes'
             onPress={handleSearchRecipes}
             />
 
