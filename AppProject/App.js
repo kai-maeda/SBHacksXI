37b4bdf1819a5bec667b6ab9cef0fe698e308113
evanja57 <evanja57@gmail.com>
2023-05-14 09:47:41 -0700

    fixing api key

--- AppProject/App.js
+++ AppProject/App.js
@@ -8,8 +8,8 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator> 
-        <Stack.Screen name='Home' component={HomeScreen} />
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={HomeScreen}/>
         <Stack.Screen name='Recipe Details' component={RecipeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
